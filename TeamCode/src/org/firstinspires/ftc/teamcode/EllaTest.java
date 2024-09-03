07528b138581d227b89ff2c27437e8a4e77dc8cd
epaletta27 <epaletta27@stjosephacademy.org>
2024-09-03 16:21:43 -0500

    Added motors

--- TeamCode/src/org/firstinspires/ftc/teamcode/EllaTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/EllaTest.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "EllaTest")
 
@@ -8,7 +10,16 @@ public class EllaTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        double leftStickY = 0.0;
+        //Get the motors
+        DcMotor front_left = hardwareMap.dcMotor.get("front_left_motor");
+        DcMotor front_right = hardwareMap.dcMotor.get("front_right_motor");
+        DcMotor back_left = hardwareMap.dcMotor.get("back_left_motor");
+        DcMotor back_right = hardwareMap.dcMotor.get("back_right_motor");
+
+        //Reverse the motors on left side
+        front_left.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left.setDirection(DcMotorSimple.Direction.REVERSE);
+
         int buttonPressCount = 0;
         boolean buttonPressed = false;
 
@@ -16,7 +27,16 @@ public class EllaTest extends LinearOpMode {
 
         while(opModeIsActive()) {
 
-            leftStickY = -gamepad1.left_stick_y;
+            //Store the values
+            double leftStickY = -gamepad1.left_stick_y;
+            double leftStickX = gamepad1.left_stick_x;
+            double triggers = gamepad1.left_trigger - gamepad1.right_trigger;
+
+            //Set the power on motors
+            front_left.setPower(leftStickY + leftStickX - triggers);
+            front_right.setPower(leftStickY - leftStickX + triggers);
+            back_left.setPower(leftStickY - leftStickX - triggers);
+            back_right.setPower(leftStickY + leftStickX + triggers);
 
            /* if (gamepad1.a) {
                 buttonPressCount++;
